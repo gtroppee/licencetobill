4dbc7d2dbfb2eee9f5b2d02a01ae09074b1ef855
gtroppee <gtroppee@gmail.com>
2014-09-01 11:20:58 +0200

    return nil if no content is fetched fron the API

--- lib/licencetobill/manager.rb
+++ lib/licencetobill/manager.rb
@@ -1,6 +1,7 @@
 module LicenceToBill
   class Manager
     def self.return_collection(klass, params)
+      return if params.parsed_response.nil?
       params = JSON.parse(params.body) unless params.kind_of?(Array)
       if params.kind_of?(Array)
         params.map { |hash| klass.new(hash) }
