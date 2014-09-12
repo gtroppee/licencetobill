e86b6dcbe29c56e79dcbc426e2646e7a238674f2
gtroppee <gtroppee@gmail.com>
2014-09-12 12:19:14 +0200

    default to empty array when no response

--- lib/licencetobill/manager.rb
+++ lib/licencetobill/manager.rb
@@ -1,13 +1,11 @@
 module LicenceToBill
   class Manager
     def self.return_collection(klass, params)
-      unless params.kind_of?(Array)
-        return if params.parsed_response.nil?
-        params = JSON.parse(params.body) 
-      end
       if params.kind_of?(Array)
         params.map { |hash| klass.new(hash) }
       else
+        return [] if params.parsed_response.nil?
+        params = JSON.parse(params.body)
         [klass.new(params)]
       end
     end
